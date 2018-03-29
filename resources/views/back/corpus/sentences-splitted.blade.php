0618fa64883510c50ef6097bc2d2e30109d26d55
Nicolas Lefebvre <nicolas.lefebvre@inria.fr>
2018-03-29 22:33:10 +0200

    force to give a sentid prefix

--- resources/views/back/corpus/sentences-splitted.blade.php
+++ resources/views/back/corpus/sentences-splitted.blade.php
@@ -3,7 +3,7 @@
 @section('main')
 
 @if(isset($sentences_splitted))
-	{!! Form::open(['url' => 'corpus/parse', 'method' => 'post', 'role' => 'form', 'target'=>'parse']) !!}
+	{!! Form::open(['url' => 'corpus/parse', 'method' => 'post', 'role' => 'form', 'target'=>'parse', 'id'=>'form-parse']) !!}
 	    <div class="col-md-10 offset-md-1">
 			<textarea style="width:100%;height:500px;" name="text">{!! $sentences_splitted !!}</textarea>
 	    </div>
@@ -19,7 +19,7 @@
 	      <option value="{{ $parser }}">{{ $parser }}</option>
 	    @endforeach
 	  </select>
-		Sentid prefix : <input type="text" value="{{ $url }}" name="url" />
+		Sentid prefix : <input type="text" value="{{ $url }}" name="url" id="sentid_prefix"/>
 		{!! Form::submit('Parse', null,['class' => 'btn btn-success']) !!}	  
 
 	</div>
@@ -27,5 +27,21 @@
 <iframe name="parse" style="width:100%;" frameborder="0" scrolling="no" onload="resizeIframe(this)" />	
 @endif
 
+
+
+@stop
+
+@section('scripts')
+<script type="text/javascript">
+$('#form-parse').submit(function(event){
+  event.preventDefault();
+  if($('#sentid_prefix').val()==''){
+  	alert('Veuillez renseigner le sentid prefix');
+  } else {
+  	$( "#form-parse" ).submit();
+  }
+});
+
+</script>
 @stop
 
