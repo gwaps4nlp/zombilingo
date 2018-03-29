75ab61615d1886670acdbdbe678dd9c644cff4d9
Nicolas Lefebvre <nicolas.lefebvre@inria.fr>
2018-03-29 18:11:49 +0200

    js backoffice

--- resources/views/back/master.blade.php
+++ resources/views/back/master.blade.php
@@ -27,7 +27,7 @@ use Gwaps4nlp\Core\Models\ConstantGame;
             @include('js.data-js')
         </script>
         <script src="{{ asset('js/socket.io.js') }}"></script>
-        <script src="{{ asset(mix("build/js/app.js")) }}"></script>
+        <script src="{{ asset(mix("build/js/all.js")) }}"></script>
 		
 		@yield('scripts')
 
