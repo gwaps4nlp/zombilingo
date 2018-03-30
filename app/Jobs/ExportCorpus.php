268ec4d1084884d80ac6b2acf9047e78fa0451b2
Nicolas Lefebvre <nicolas.lefebvre@inria.fr>
2018-03-30 12:44:53 +0200

    fix export

--- app/Jobs/ExportCorpus.php
+++ app/Jobs/ExportCorpus.php
@@ -5,12 +5,11 @@ namespace App\Jobs;
 use App\Jobs\Job;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Services\ConllExporter;
 use App\Models\Corpus;
 
-class ExportCorpus extends Job implements SelfHandling, ShouldQueue
+class ExportCorpus extends Job implements ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 	
