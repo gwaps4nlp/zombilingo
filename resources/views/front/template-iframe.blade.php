fd11cea87797dbf2efa935a14552895019bcff8c
Nicolas Lefebvre <nicolas.lefebvre@inria.fr>
2018-03-30 11:07:37 +0200

    resize iframe

--- resources/views/front/template-iframe.blade.php
+++ resources/views/front/template-iframe.blade.php
@@ -11,7 +11,36 @@
     </head>
     <body class="{{ App::environment('local')?'test':'' }}">
 		@yield('main')
-    <script src="{{ asset(mix("build/js/all.js")) }}" />
+    <script>
+      function resizeIframe(obj) {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        var arrFrames = parent.parent.document.getElementsByTagName("IFRAME");
+        for (var i = 0; i < arrFrames.length; i++) {
+          if (arrFrames[i].name != obj.name) {
+            resizeIframe(arrFrames[i]);
+            }
+        }
+      }
+      function resizeIframe2(obj) {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        var arrFrames = parent.parent.parent.document.getElementsByTagName("IFRAME");
+        for (var i = 0; i < arrFrames.length; i++) {
+          if (arrFrames[i].name != obj.name) {
+            resizeIframe2(arrFrames[i]);
+            }
+        }
+      }
+      function resizeIframeAgain(obj) {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        var arrFrames = parent.document.getElementsByTagName("IFRAME");
+        for (var i = 0; i < arrFrames.length; i++) {
+          if (arrFrames[i].name != obj.name) {
+            resizeIframeAgain(arrFrames[i]);
+            }
+        }
+      }
+    </script>
+    <script src="{{ asset(mix("build/js/app.js")) }}" />
 		@yield('scripts')
     </body>
 </html>
