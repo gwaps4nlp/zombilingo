7a38a0c914c166cabca8bf887b862fd910c9ea88
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2019-01-15 17:55:14 +0100

    update doc of the relative relations

--- resources/views/help/fr/mod_rel.php
+++ resources/views/help/fr/mod_rel.php
@@ -3,26 +3,28 @@ Trouve le mot principal de la relative (la partie de la phrase qui modifie le mo
 Si la relative est introduite par un pronom relatif différent de <em>dont</em> (<em>qui</em>, <em>que</em>, <em>où</em>, <em>auquel</em>, ...), le mot à trouver est un verbe.
 <em>
   <ul>
-	<li><span class="sentence">…une <span class=highlight>forge</span> qui <span class=solution>connut</span> son apogée au XIXe siècle.</span></li>
-	<li><span class="sentence">…un <span class=highlight>pays</span> où tout <span class=solution>est</span> blanc.</span></li>
-	<li><span class="sentence"><span class=highlight>Ce</span> que nous <span class=solution>voulons</span> surtout, c'est économiser les combustibles fossiles.</span></li>
-	<li><span class="sentence">…des <span class=highlight>dysfonctionnements</span> auxquels il <span class=solution>convient</span> de remédier…</span></li>
+    <li><span class="sentence">…une <span class=highlight>forge</span> qui <span class=solution>connut</span> son apogée au XIXe siècle.</span></li>
+    <li><span class="sentence">…un <span class=highlight>pays</span> où tout <span class=solution>est</span> blanc.</span></li>
+    <li><span class="sentence"><span class=highlight>Ce</span> que nous <span class=solution>voulons</span> surtout, c'est économiser les combustibles fossiles.</span></li>
+    <li><span class="sentence">…des <span class=highlight>dysfonctionnements</span> auxquels il <span class=solution>convient</span> de remédier…</span></li>
   </ul>
 </em>
 Si le temps est composé (auxiliaire + verbe), c'est le verbe qu'il faut sélectionner
 <em>
   <ul>
-	<li><span class="sentence">…de délicieux <span class=highlight>pains</span> au chocolat qu'ils ont <span class=solution>dégustés</span> à l'heure du goûter…</span></li>
+    <li><span class="sentence">…de délicieux <span class=highlight>pains</span> au chocolat qu'ils ont <span class=solution>dégustés</span> à l'heure du goûter…</span></li>
   </ul>
 </em>
 
-Si la relative est introduite par <em>dont</em>, alors la mot à trouver est un nom ou un pronom.
+Si la relative est introduite par <em>dont</em>, alors la mot à trouver peut être un verbe mais aussi un nom ou un pronom.
 <em>
   <ul>
-	<li><span class="sentence">…plusieurs <span class=highlight>centaines</span> de milliers d'Irakiens, dont quelque 500000 <span class=solution>enfants</span>…</span></li>
-	<li><span class="sentence">…plusieurs <span class=highlight>proches</span> du chef de l'État, dont sa  <span class=solution>fille</span> Claude Chirac.</span></li>
-	<li><span class="sentence">…30 <span class=highlight>mois</span> dont 6 <span class=solution>mois</span> fermes.</span></li>
-	<li><span class="sentence">…30 <span class=highlight>mois</span> dont <span class=solution>6</span> fermes.</span></li>
+    <li><span class="sentence">…un <span class=highlight>club</span> dont je <span class=solution>dois</span> taire le nom</span></li>
+    <li><span class="sentence">…un de ces <span class=highlight>épisodes</span> amusants dont elle <span class=solution>raffole</span> taire le nom</span></li>
+    <li><span class="sentence">…plusieurs <span class=highlight>centaines</span> de milliers d'Irakiens, dont quelque 500000 <span class=solution>enfants</span>…</span></li>
+    <li><span class="sentence">…plusieurs <span class=highlight>proches</span> du chef de l'État, dont sa <span class=solution>fille</span> Claude Chirac.</span></li>
+    <li><span class="sentence">…30 <span class=highlight>mois</span> dont 6 <span class=solution>mois</span> fermes.</span></li>
+    <li><span class="sentence">…30 <span class=highlight>mois</span> dont <span class=solution>6</span> fermes.</span></li>
   </ul>
 </em>
 
