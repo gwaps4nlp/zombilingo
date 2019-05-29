0334dde441e0a80a8d1b8fb857e088f7fd86b0a0
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2019-05-29 18:30:41 +0200

    add CPER logos

--- resources/views/lang/fr/informations.blade.php
+++ resources/views/lang/fr/informations.blade.php
@@ -28,18 +28,20 @@ Avant de jouer, lisez la {!! link_to('charter','charte') !!} de ZombiLingo.
 	<li>Valentin Stern&nbsp;: développement du premier prototype (ne participe plus)</li>
 </ul>
 
-<h2>Soutiens</h2>
+<h2>Soutiens</h2
+
+		<p>Le serveur est hébergé sur l'infrastructure du <a href="http://lchn.fr">CPER LCHN « <strong>L</strong>angues, <strong>C</strong>onnaissances &amp; <strong>H</strong>umanités <strong>N</strong>umériques » </a></p>
 		<div class="row">
 			<div class="col-md-2 col-md-offset-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
 				<a href="http://www.paris-sorbonne.fr/" target="_blank">
 					{!! Html::image('img/logo_sorbonne.png','Logo Sorbonne') !!}
 				</a>
 			</div>
-			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+			<!-- <div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
 				<a href="http://www.loria.fr/" target="_blank">
 					{!! Html::image('img/logo_loria.png','Logo Loria') !!}
 				</a>
-			</div>
+			</div> -->
 			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
 				<a href="http://www.inria.fr/" target="_blank">
 					{!! Html::image('img/logo_inria.png','Logo INRIA') !!}
@@ -48,6 +50,21 @@ Avant de jouer, lisez la {!! link_to('charter','charte') !!} de ZombiLingo.
 			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
 				<a href="http://www.culturecommunication.gouv.fr/Politiques-ministerielles/Langue-francaise-et-langues-de-France" target="_blank">
 					{!! Html::image('img/logo_MCC.png','Logo Ministère de la Culture') !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 25px; padding-bottom: 20px;" >
+				<a href="http://www.enseignementsup-recherche.gouv.fr/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/etat.gif','Logo Ministère Enseignement Supérieur', array(  'height' =>90 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 50px; padding-bottom: 20px;" >
+				<a href="http://www.lorraine.eu/accueil.html" target="_blank">
+					{!! Html::image('http://lchn.fr/img/Region.jpeg','Logo Ministère de la Région Lorraine', array(  'height' =>40 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
+				<a href="http://europe-en-lorraine.eu/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/feder.jpeg','Logo FEDER', array(  'height' =>90 )) !!}
 				</a>
 			</div>
 		</div>
