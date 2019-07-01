89fd34f3bf530f6345dcda6ea63c95d50d33f390
Karen Fort <kfort@schplaf.org>
2019-07-01 10:14:39 +0200

    added info for zlud

---
+++ resources/views/lang/fr/informations_zlud.blade.php
@@ -0,0 +1,79 @@
+<h1 class="text-center">À propos de ZombiLUDik</h1>
+
+<h2>Quoi ?</h2>
+<p>
+ZombiLUDik est un jeu frère de ZombiLingo, dont le but est d'annoter des corpus en syntaxe de dépendances (selon le formalisme Universal Dependencies) et de récolter d'autres données langagières.
+Les points (des cerveaux) peuvent être gagnés non seulement en annotant beaucoup de phrases, mais surtout en les annotant correctement.
+</p>
+
+<p>
+ZombiLUDik propose également d'autres moyens de gagner des points, qui sont laissés à la découverte du joueur.
+Ce jeu s'adresse aux amoureux de la langue et de la grammaire, ainsi qu'aux fanatiques du monde des Zombies.
+</p>
+
+<p>
+Avant de jouer, lisez la {!! link_to('charter','charte') !!} de ZombiLUDik.
+</p>
+
+
+<h2>Qui ?</h2>
+<ul>
+	<li><a href="http://karenfort.org/" target="_blank">Karën Fort</a> -- (Équipe STIH, Sorbonne Université)&nbsp;: conception et animation du jeu</li>
+	<li><a href="http://www.loria.fr/~guillaum" target="_blank">Bruno Guillaume</a> -- (Équipe Sémagramme, LORIA)&nbsp;: conception et animation du jeu</li>
+	<li>Nikola lackovic&nbsp;: adaptation depuis ZombiLingo</li>
+	<li>M2 Langues et informatique de Sorbonne Université (en particulier Diego Alves, Yann-Alan Pilatte, Jean-Baptiste Tanguy et Anaëlle Baledent)&nbsp;: adaptation depuis ZombiLingo</li>
+	<li>Nicolas Lefebvre&nbsp;: développement de ZombiLingo (dont ZombiLUDik est issu)</li>
+	<li>Charles Ancé&nbsp;: dessins</li>
+</ul>
+
+<h2>Soutiens</h2
+
+		<p>Le serveur est hébergé sur l'infrastructure du <a href="http://lchn.fr">CPER LCHN « <strong>L</strong>angues, <strong>C</strong>onnaissances &amp; <strong>H</strong>umanités <strong>N</strong>umériques » </a></p>
+		<div class="row">
+			<div class="col-md-2 col-md-offset-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://lettres.sorbonne-universite.fr" target="_blank">
+					{!! Html::image('img/logo_sorbonne.png','Logo Sorbonne') !!}
+				</a>
+			</div>
+			<!-- <div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://www.loria.fr/" target="_blank">
+					{!! Html::image('img/logo_loria.png','Logo Loria') !!}
+				</a>
+			</div> -->
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://www.inria.fr/" target="_blank">
+					{!! Html::image('img/logo_inria.png','Logo INRIA') !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
+				<a href="http://www.culturecommunication.gouv.fr/Politiques-ministerielles/Langue-francaise-et-langues-de-France" target="_blank">
+					{!! Html::image('img/logo_MCC.png','Logo Ministère de la Culture') !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 25px; padding-bottom: 20px;" >
+				<a href="http://www.enseignementsup-recherche.gouv.fr/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/etat.gif','Logo Ministère Enseignement Supérieur', array(  'height' =>90 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 50px; padding-bottom: 20px;" >
+				<a href="http://www.lorraine.eu/accueil.html" target="_blank">
+					{!! Html::image('http://lchn.fr/img/Region.jpeg','Logo Ministère de la Région Lorraine', array(  'height' =>40 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
+				<a href="http://europe-en-lorraine.eu/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/feder.jpeg','Logo FEDER', array(  'height' =>90 )) !!}
+				</a>
+			</div>
+		</div>
+
+<h2>Publications</h2>
+Pour citer ZombiLingo (en attendant une publication plus spécifique sur ZombiLUDik) :<br />
+<ul>
+	<li>Karën Fort, Bruno Guillaume et Nicolas Lefebvre.
+		<a href="https://hal.inria.fr/hal-01378980">Crowdsourcing Complex Language Resources: Playing to Annotate Dependency Syntax.</a>
+		<i>International Conference on Computational Linguistics (COLING)</i>, Osaka, Japon, décembre 2016.
+	</li>
+</ul>
+
+<h2 id="export">Export</h2>
