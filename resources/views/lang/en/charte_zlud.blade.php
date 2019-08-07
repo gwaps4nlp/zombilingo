4c3bb1f2cc1e8cf6726df10b2dc30dd882892e26
Karen Fort <kfort@schplaf.org>
2019-08-07 15:33:31 +0200

    translations added for ZLUD

---
+++ resources/views/lang/en/charte_zlud.blade.php
@@ -0,0 +1,112 @@
+<h2>Charter of admission to ZombiLUDik</h2>
+
+<p>
+ZombiLUDik is a spinoff of <a href="https://zombilingo.org/" target="_blank">ZombiLingo</a>, a free game created as part of a research project by <a href="http://www.schplaf.org/kf/index_en.php" target="_blank">Karën Fort</a> (associate prof. at Univ. Paris-Sorbonne)
+and <a href="http://www.loria.fr/~guillaum" target="_blank">Bruno Guillaume</a> (researcher at Inria Grand Est),
+at  <a href="http://www.loria.fr" target="_blank">LORIA</a>.
+The aim of the project is to build language resources (lexicons and annotated corpora) for Natural Language Processing (NLP) applications.
+The language data is produced by the players' activity on ZombiLUDik.
+</p>
+<p>
+The produced data is freely {!! link_to('information','available',['target'=>'_blank']) !!} under various CC licences (depending on the licence of the original corpus), which are specified in each resource.
+The source code of ZombiLingo est freely available on GitHub under a CeCILL 2.1 licence.
+</p>
+<p>
+The present charter aims to allow players to participate in an enjoyable environment, provided they respect the rules detailed below.
+</p>
+<p>
+Any breach or abuse of the rules will lead to penalties, including the permanent banishment of the faulty player.
+Players are expected to be respectful of others and to allow everyone to enjoy the game.
+</p>
+<h2>
+(1) Registration
+</h2>
+<p>
+ZombiLingo is a free game and registration is open to anybody provided they have internet access.
+Messages can be sent among players if the evolution of the game will include this feature.
+The corpora dealt with in ZombiLUDik may contain texts that are not suitable for persons under 16.
+</p>
+<h2>
+(2) Personal Data
+</h2>
+<p>
+We only collect information for the game to function properly. This information is automatically processed. Some of it is personal (password, Email addresses, etc), others (answers given during the game, ranking, etc.) may be shown to other people within the game or for research purposes.
+We will NOT distribute or sell any personal data from the players outside the game itself (information presented in the leaderboard, display of the results of one game, etc).
+s</p>
+<p>
+All the data is accessible by all the members of the development team without restriction.
+The team will use the collected language data for research purposes.
+This data does not contain personal information that can be traced back to the author.
+</p>
+<p>
+Players can delete their account at anytime, with all the information contained in the database. However, the data created by the player and his or her actions in the game will be kept if they are useful for the language resource.
+</p>
+<p>
+In accordance with the French law on «&nbsp;informatique et libertés&nbsp;» (computer science and freedom) from January 6th, 1978 modified in 2004, you may access and modify information about you, by writing to:
+</p>
+
+<p>
+<a href="mailto:bruno.guillaume@loria.fr">Bruno Guillaume</a>, LORIA UMR 7503
+Campus Scientifique, BP 239
+F-54506 Vandœuvre-lès-Nancy Cedex
+</p>
+
+<h2>
+(3) Language Data
+</h2>
+
+<p>
+Some texts, terms or questions asked in ZombiLUDik might be considered as vulgar or offensive by some players. As the aim of ZombiLUDik is to create wide coverage language data, this information will in general be kept in the game. However, we encourage players to bring to our attention any inappropriate content.
+</p>
+<h2>
+(4) Accounts
+</h2>
+<p>
+Selling accounts or game elements in exchange for money or material belongings is strickly forbidden.
+</p>
+<h2>
+(5) Automatization
+</h2>
+<p>
+An account must be controlled by a player: it is strickly forbidden to automatize actions by any means (script, program, website, etc).
+</p>
+<h2>
+(6) Bugs
+</h2>
+<p>
+ZombiLUDik is not free from bugs and inconsistencies. The players are asked to report (by mail or through the forum) as soon as possible any problem they identify. Any abusive or voluntary usage of a bug or flaw in the code of the game will be punished and the account of the player may be deleted. Compensations can be decided by administrators, at their convenience.
+</p>
+<h2>
+(7) Navigation cookies
+</h2>
+<p>
+We need to use cookies for the game to function correctly.
+Such a cookie does not allow us to identify you, however, it registers information regarding your navigation on our site (the pages you have visited, the date and time of the visit, etc.) that we will be able to read when you visit us next time. This information will remain several months on your computer.
+You may oppose the setting of cookies, in which case ZombiLUDik will not function properly.
+To oppose the setting of cookies, you should configure your computer in the following way:
+
+On Microsoft Internet Explorer 6.0: 1. select "Tools", then "Internet Options". 2. click on the "Confidentiality" tab 3. select the desired level with the cursor.
+
+On Firefox: 1. select "Edit">"Preferences"; 2. click on the option "Privacy"; 3. "Delete specific cookies" .
+
+On Opera 6.0 and beyond: 1. select "File">"Preferences"; 2. Privacy.
+</p>
+<h2>
+(8) Addiction
+</h2>
+<p>
+If you become aware of important modifications in your private day-to-day life, like withdrawal syndrom, loss of control concerning games,  conflicts with relatives, you may suffer from game addition. We advice you to contact hotline dedicated to that type of problem like <i>Joueurs Info Service</i> in France (<a target="_blank" href ="http://www.joueurs-info-service.fr">http://www.joueurs-info-service.fr</a>).
+</p>
+
+<h2>
+(9) Charter of Admission
+</h2>
+<p>
+The present charter has to be read and accepted in its entirety before playing ZombiLUDik.
+It may be modified by the administrators of the game.
+The players may be asked to explicitely accept the modifications of the charter to be able to continue playing.
+
+Karën Fort and Bruno Guillaume - Administrators - Contacts: karen dot fort at sorbonne-universite dot fr and bruno dot guillaume at loria dot fr | <a target="_blank" href="http://www.jeuxdemots.org">Sources of the JDM charter (FR)</a> - <a target="_blank" href="http://www.starshine-online.com/">SSO</a> - <a target="_blank" href="http://www.mountyhall.com/">MH</a> - <a target="_blank" href="http://www.cnil.fr/">French CNIL</a>
+
+Last update: August 2019
+</p>
---
+++ resources/views/lang/en/informations_zlud.blade.php
@@ -0,0 +1,80 @@
+<h1 class="text-center">About ZombiLUDik</h1>
+
+<h2>What is ZombiLUDik?</h2>
+<p>
+ZombiLUDik is a spinoff of ZombiLingo, a Game With A Purpose which aims to annotate corpora with dependency syntax relations as well as to gather other language data.
+Points (brains) are won when annotating sentences, and annotating them well will help you gain even more. Conversely, bad annotations will generate a waste of time and points.
+</p>
+
+<p>
+ZombiLUDik offers other ways to win points that you will discover while playing.
+This game is intended for people interested in language and grammar, as well as zombie addicts.
+</p>
+
+<p>
+Before playing, please read our {!! link_to('charter','charter') !!}.
+</p>
+
+
+<h2>Who is involved?</h2>
+<ul>
+	<li><a href="http://karenfort.org/" target="_blank">Karën Fort</a> -- (STIH team, Sorbonne Université): research and design of the game</li>
+	<li><a href="http://www.loria.fr/~guillaum" target="_blank">Bruno Guillaume</a> -- (Sémagramme team, LORIA): research and design of the game</li>
+	<li>Nikola lackovic&nbsp;: adaptation from ZombiLingo</li>
+	<li>M2 Langues et informatique de Sorbonne Université (in particular Diego Alves, Yann-Alan Pilatte, Jean-Baptiste Tanguy and Anaëlle Baledent)&nbsp;: adaptation from ZombiLingo</li>
+	<li>Nicolas Lefebvre&nbsp;: development of ZombiLingo</li>
+	<li>Charles Ancé&nbsp;: drawings</li>
+</ul>
+
+<h2>Fundings</h2>
+		<p>The server is hosted on the <a href="http://lchn.fr">CPER LCHN « <strong>L</strong>angues, <strong>C</strong>onnaissances &amp; <strong>H</strong>umanités <strong>N</strong>umériques » </a> infrastructure</p>
+		<div class="row">
+			<div class="col-md-2 col-md-offset-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://lettres.sorbonne-universite.fr" target="_blank">
+					{!! Html::image('img/logo_sorbonne.png','Logo Sorbonne') !!}
+				</a>
+			</div>
+			<!-- <div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://www.loria.fr/" target="_blank">
+					{!! Html::image('img/logo_loria.png','Logo Loria') !!}
+				</a>
+			</div> -->
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;">
+				<a href="http://www.inria.fr/" target="_blank">
+					{!! Html::image('img/logo_inria.png','Logo INRIA') !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
+				<a href="http://www.culturecommunication.gouv.fr/Politiques-ministerielles/Langue-francaise-et-langues-de-France" target="_blank">
+					{!! Html::image('img/logo_MCC.png','Logo Ministère de la Culture') !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 25px; padding-bottom: 20px;" >
+				<a href="http://www.enseignementsup-recherche.gouv.fr/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/etat.gif','Logo Ministère Enseignement Supérieur', array(  'height' =>90 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 50px; padding-bottom: 20px;" >
+				<a href="http://www.lorraine.eu/accueil.html" target="_blank">
+					{!! Html::image('http://lchn.fr/img/Region.jpeg','Logo Ministère de la Région Lorraine', array(  'height' =>40 )) !!}
+				</a>
+			</div>
+			<div class="col-md-2 text-center" style="background-color:white; padding-top: 20px; padding-bottom: 20px;" >
+				<a href="http://europe-en-lorraine.eu/" target="_blank">
+					{!! Html::image('http://lchn.fr/img/feder.jpeg','Logo FEDER', array(  'height' =>90 )) !!}
+				</a>
+			</div>
+		</div>
+
+<h2>Publications</h2>
+To cite ZombiLingo (no publication specific to ZombiLUDik yet):<br />
+<ul>
+	<li>Karën Fort, Bruno Guillaume and Nicolas Lefebvre.
+		<a href="https://hal.inria.fr/hal-01378980">Crowdsourcing Complex Language Resources: Playing to Annotate Dependency Syntax.</a>
+		<i>International Conference on Computational Linguistics (COLING)</i>, Osaka, Japan, December 2016.
+	</li>
+
+</ul>
+
+<h2 id="export">Export</h2>
+
