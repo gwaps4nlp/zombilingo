7956730b6b883bf009c06a3711d3ef5f1fdb99ca
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 13:02:03 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_aux_pass.php
+++ resources/views/help/fr/ud_aux_pass.php
@@ -1,21 +1,24 @@
+<p>Attention, ici il ne faut sélectionner que l'auxiliaire passif, si l'auxiliaire ne permet pas de rendre compte d'une phrase au passif, il faut mettre croix d'os</p>
 <p>Un auxiliaire passif n‘est pas le verbe principal d‘une proposition mais contient l‘information que le sujet subit l‘action exprimée par le verbe principal.</p>
-<p>Seul <em>être</em> peut être utilisé comme auxiliaire passif.</p>
+<p>Seul <em>être</em> peut être utilisé comme auxiliaire passif.
 <em>
   <ul>
     <li><span class="sentence">Kennedy <span class="solution">fut</span> <span class="highlight">assassiné</span></span></li>
   </ul>
 </em>
-<p>Dans un cas particulier on peut avoir deux auxiliaires rattachés au même mot&nbsp;:</p>
+</p>
+<p>Dans un cas particulier on peut avoir deux auxiliaires rattachés au même mot&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Kennedy a <span class="solution">été</span> <span class="highlight">assassiné</span> (ici a est l‘auxiliaire, été est l‘auxiliaire passif et assassiné est le verbe principal)</span></li>
   </ul>
 </em>
-<p>Attention, ici il ne faut sélectionner que l'auxiliaire passif, si l'auxiliaire ne permet pas de rendre compte d'une phrase au passif, il faut mettre croix d'os</p>
+</p>
 <p>Dans tous les cas c’est le verbe être qui est l’auxiliaire passif.</p>
-<p>Il n’est possible d’avoir un auxiliaire passif qu’avec les verbes transitifs, ce sont des verbes qui admettent un ou plusieurs compléments d’objets.</p>
+<p>Il n’est possible d’avoir un auxiliaire passif qu’avec les verbes transitifs, ce sont des verbes qui admettent un ou plusieurs compléments d’objets.
 <em>
   <ul>
     <li><span class="sentence">Le détroit <span class="solution">est</span> <span class="highlight">traversé</span> par deux ponts routiers&nbsp;: le Carquinez Bridge, sur lequel passe l‘Interstate 80, et le Benicia-Martinez Bridge emprunté par l‘Interstate 680.</span></li>
   </ul>
 </em>
+</p>
