47dbcbcdf5e28139c679679bb041d8bc96a42988
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 13:06:06 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_dislocated.php
+++ resources/views/help/fr/ud_dislocated.php
@@ -1,44 +1,46 @@
 <p>Dans le cas d’un énoncé oral en français, nous avons tendance à répéter certaines informations concernant ce dont on parle.</p>
 
-<p>Le cas où deux noms sont relations l’un avec l’autre&nbsp;:</p>
+<p>Le cas où deux noms sont relations l’un avec l’autre&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Mais qu‘importe&nbsp;: ce <span class="solution">soir</span> c‘est LE <span class="highlight">soir</span> !</span></li>
   </ul>
 </em>
-
-<p>Voici un exemple où un pronom est en relation avec un verbe&nbsp;:</p>
+</p>
+<p>Voici un exemple où un pronom est en relation avec un verbe&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class="solution">Lui</span> il avait <span class="highlight">passé</span> les quatre nuits ou trois nuits à ramper dans les décombres</span></li>
   </ul>
 </em>
-
+</p>
 <p>Dans ce cas précis, on fait référence à une personne de deux manières avec le mot : «&nbsp;il&nbsp;» et avec le mot&nbsp;: «&nbsp;lui&nbsp;», en français la même phrase sans le mot «&nbsp;lui&nbsp;» est également possible sans altérer le sens de la phrase, on dit alors que l’on a une information redondante concernant le sujet.</p>
 
-<p>Un autre exemple peut concerner non pas des personnes mais également des objets, des évènements, toute chose qui n’est pas humaine.</p>
+<p>Un autre exemple peut concerner non pas des personnes mais également des objets, des évènements, toute chose qui n’est pas humaine.
 
 <em>
   <ul>
     <li><span class="sentence">c’est déjà <span class="highlight">arrivé</span> <span class="solution">ça</span></span></li>
   </ul>
 </em>
+</p>
 <p>Dans ce cas là ce qui apparaît deux fois dans l’énoncé, c’est donc un évènement.</p>
 
-<p>Dans le cas où la chose est expressément nommer on peut aisément reconnaître le mot d’origine, ce cas est celui où un nom est en relation avec le verbe&nbsp;:</p>
+<p>Dans le cas où la chose est expressément nommer on peut aisément reconnaître le mot d’origine, ce cas est celui où un nom est en relation avec le verbe&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">ces <span class="solution">rails</span> du tram eh ben je vais les <span class="highlight">longer.</span></span></li>
   </ul>
 </em>
+</p>
 <p>D’une certaine façon nous répétons l’information qu’il y a des rails que l’on longe avec le mot «&nbsp;les&nbsp;».</p>
 
-<p>Un autre exemple de cas de relation de dislocation entre un verbe et un nom&nbsp;:</p>
+<p>Un autre exemple de cas de relation de dislocation entre un verbe et un nom&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Admirateur passionné de Wellington, le <span class="solution">vainqueur</span> de Waterloo, comme son héros il s‘<span class="highlight">enroule</span> autour du cou une large cravate blanche qui finit par lui couvrir le menton, mode passée de son temps mais qu‘il n‘abandonne jamais sous prétexte d‘une gorge fragile.</span></li>
   </ul>
 </em>
-
+</p>
 <p>Ici, on voit que la redondance du pronom «&nbsp;il&nbsp;» est présente et n’ajoute pas d’information supplémentaire à la phrase.</p>
 
