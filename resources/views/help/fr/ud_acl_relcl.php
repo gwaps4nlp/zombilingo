fd167ba0a070037ab2685cb2d581005f6cecd462
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 12:57:34 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_acl_relcl.php
+++ resources/views/help/fr/ud_acl_relcl.php
@@ -1,23 +1,26 @@
 <p>La relation concerne un nom, un nom propre ou un pronom en liaison avec le verbe qui les suit.</p>
 
-</p>exemple pour le cas du nom&nbsp;:</p>
+</p>exemple pour le cas du nom&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Un <span class="solution">client</span> qui <span class="highlight">mangeait</span> des bonbons dans la boulangerie s’est fait réprimander.</span></li>
   </ul>
 </em>
+</p>
 
-<p>exemple pour le cas d’un nom propre&nbsp;:</p>
+<p>exemple pour le cas d’un nom propre&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Charlie Hebdo avait néanmoins viré le dessinateur <span class="solution">Siné</span> qui avait <span class="highlight">parlé</span> du fils de Sarkozy.</span></li>
   </ul>
 </em>
+</p>
 
-<p>exemple pour le cas d’un pronom&nbsp;:</p>
+<p>exemple pour le cas d’un pronom&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Ses poèmes sont écrits avec une facilité, une grâce et un abandon qui rappellent la manière de Cats, <span class="solution">celui</span> de tous les poètes des provinces septentrionales qui <span class="highlight">eut</span> le plus de lecteurs et d‘imitateurs en Flandre et au Brabant.</span></li>
   </ul>
 </em>
+ </p>
 <p>Il faut noter qu’entre le nom, pronom ou nom propre il y a systématiquement dans ce cas une séparation avec une conjonction de type «&nbsp;qui, que, quoi, dont, où&nbsp;» et le mot à sélectionner se trouve toujours avant ces mots.</p>
