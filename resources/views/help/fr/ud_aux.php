a247599e6e908e4ba3f298b7be7f573d33daf0d3
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 13:00:16 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_aux.php
+++ resources/views/help/fr/ud_aux.php
@@ -1,25 +1,27 @@
-<p>Les auxiliaires</span> sont des verbes utilisés pour former un temps composé lorsqu‘ils sont associés à des verbes au participe passé. Le français utilise deux auxiliaires: <em>être et avoir</em>.</p>
+<p>Les auxiliaires</span> sont des verbes utilisés pour former un temps composé lorsqu‘ils sont associés à des verbes au participe passé. Le français utilise deux auxiliaires: <em>être et avoir</em>.
 <em>
   <ul>
     <li><span class="sentence">Max <span class="solution">a</span> <span class="highlight">marché</span> pendant des heures</span></li>
   </ul>
 </em>
-<p>Le verbe associé à l‘auxiliaire peut lui aussi être être ou avoir&nbsp;:</p>
+</p>
+<p>Le verbe associé à l‘auxiliaire peut lui aussi être être ou avoir&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Il <span class="solution">a</span> <span class="highlight">eu</span> alors comme élèves&nbsp;: les israéliens Galit Chait/Sergei Sakhnovski, les britanniques Sinead Kerr/John Kerr…</span></li>
   </ul>
 </em>
+</p>
 <p>Pour retrouver l’auxiliaire il faut changer le temps de la phrase, s’assurer que notre auxiliaire n’est plus présent mais également que le sens de la phrase n’a pas été modifié par notre changement de temps.<br /> Dans le premier exemple, on met la phrase au présent et cela donne : «&nbsp;Max marche pendant des heures&nbsp;» outre le changement de temps, le sens reste le même. On peut ainsi dire que l’auxiliaire à retrouver est «&nbsp;a&nbsp;».</p>
-<p>Cela est important car il ne faut pas confondre l’auxiliaire simple et l’auxiliaire passif. Les deux peuvent cependant coexister au sein de la phrase et il convient de les identifier correctement&nbsp;:</p>
+<p>Cela est important car il ne faut pas confondre l’auxiliaire simple et l’auxiliaire passif. Les deux peuvent cependant coexister au sein de la phrase et il convient de les identifier correctement&nbsp;:
 <em>
   <ul>
     <li>Josiane Bernard a été élue en 2001 conseillère générale du canton de Bagnolet et vice-présidente du Conseil général de Seine-Saint-Denis.</li>
   </ul>
 </em>
+</p>
 <p>Il existe dans la phrase ci-dessus, un auxiliaire simple et un auxiliaire passif. Si l’on met cette phrase au présent cela donne&nbsp;: «&nbsp;Josiane Bernard est élue en 2001&nbsp;» le sens de la phrase ne change pas et notre auxiliaire est correctement identifié comme étant «&nbsp;a&nbsp;».</p>
 
-<p>En revanche si l’on dit&nbsp;: «&nbsp;Josiane Bernard a élue en 2001&nbsp;», le sens de la phrase change complètement car ce que l’on a effacé n’est pas l’auxiliaire mais l’auxiliaire passif.</p>
-
-<p>Ainsi il faut se souvenir qu’en effectuant le test d’effacer un auxiliaire en changeant le temps de la phrase, cette dernière ne doit pas avoir un sens différent sinon c’est que l’on a effacé l’auxiliaire passif et non l’auxiliaire simple.</p>
+<p>En revanche si l’on dit&nbsp;: «&nbsp;Josiane Bernard a élue en 2001&nbsp;», le sens de la phrase change complètement car ce que l’on a effacé n’est pas l’auxiliaire mais l’auxiliaire passif.<br />
+Ainsi il faut se souvenir qu’en effectuant le test d’effacer un auxiliaire en changeant le temps de la phrase, cette dernière ne doit pas avoir un sens différent sinon c’est que l’on a effacé l’auxiliaire passif et non l’auxiliaire simple.</p>
 
