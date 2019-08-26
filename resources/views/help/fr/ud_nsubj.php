75f0ea9324f71374e24cc16713c34297f4a09438
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 12:46:18 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_nsubj.php
+++ resources/views/help/fr/ud_nsubj.php
@@ -1,9 +1,7 @@
 <p>La relation sujet nominal désigne le lien entre le sujet et un mot de la phrase.
 Les sujets peuvent avoir différentes «&nbsp;natures&nbsp;» et les mots dont ils dépendent également.</p>
 
-
-<p>Le sujet peut être respectivement, un pronom, un nom commun ou un nom propre&nbsp;:</p>
-
+<p>Le sujet peut être respectivement, un pronom, un nom commun ou un nom propre&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class=solution>Je</span> <span class=highlight>peux</span> pas vous dire.</span></li>
@@ -11,31 +9,32 @@ Les sujets peuvent avoir différentes «&nbsp;natures&nbsp;» et les mots dont i
     <li><span class="sentence"><span class=solution>Jésus</span> ne <span class=highlight>dit</span> rien avant la fin de l'épisode</span></li>
   </ul>
 </em>
+</p>
 
-<p>Le mot surligné n'est pas obligatoirement un verbe, les sujets peuvent également dépendre de mots qui sont des adjectifs ou des noms&nbsp;:</p>
-
+<p>Le mot surligné n'est pas obligatoirement un verbe, les sujets peuvent également dépendre de mots qui sont des noms ou des adjectifs&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">L'<span class=solution>art</span> est le <span class=highlight>reflet</span> de la société.</span></li>
     <li><span class="sentence">enfin toute ma <span class=solution>famille</span> est <span class=highlight>portugaise.</span></li>
+    <li><span class="sentence>
   </ul>
 </em>
+</p>
 
-<p>on peut aussi rencontrer une coordination&nbsp;; dans ce cas, le mot à chercher est toujours celui qui apparaît <b>avant</b> la conjonction de coordination&nbsp;:</p>
-
+<p>on peut aussi rencontrer une coordination&nbsp;; dans ce cas, le mot à chercher est toujours celui qui apparaît <b>avant</b> la conjonction de coordination&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">j’ai trouvé que l’<span class="solution">enseignement</span> et la recherche <span class=highlight>allaient</span> bien ensemble généralement.</span></li>
   </ul>
 </em>
+</p>
 
-<p>Dans le cas des noms propres (présence de prénom et nom), il faut sélectionner le prénom. Dans le cas d'un nom et d'une fonction il faut choisir la fonction, respectivement&nbsp;:</p>
-
+<p>Dans le cas des noms propres (présence de prénom et nom), il faut sélectionner le prénom. Dans le cas d'un nom et d'une fonction il faut choisir la fonction, respectivement&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class=solution>Phillipe</span> Zdar est  <span class=highlight>tombé</span> depuis la fenêtre de son immeuble hier.</span></li>
      <li><span class="sentence">Le <span class=solution>coach</span> Debord a <span class=highlight>dit</span> que c’était plus possible de venir au match sans protège-tibias.</span></li>
   </ul>
 </em>
-
+</p>
 
