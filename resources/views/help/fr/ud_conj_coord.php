f8141b07dda59c56a6f30ff98dab2236fac5413b
Onevirtual <niko.lackovic@gmail.com>
2019-08-26 13:04:48 +0200

    Format modifications (more compact)

--- resources/views/help/fr/ud_conj_coord.php
+++ resources/views/help/fr/ud_conj_coord.php
@@ -1,27 +1,28 @@
 <p>Dans le premier cas, les deux mots sont coordonnés par le mot «&nbsp;et&nbsp;», en français ce dernier est une conjonction de coordination.</p>
 
-<p>Pour le premier cas le plus fréquent avec la conjonction «&nbsp;et&nbsp;» qui coordonnent deux noms&nbsp;:</p>
+<p>Pour le premier cas le plus fréquent avec la conjonction «&nbsp;et&nbsp;» qui coordonnent deux noms&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class="solution">Equipiers</span> et <span class="highlight">adversaires</span> sont sur la même longueur d'ondes.</span></li>
   </ul>
 </em>
+</p>
 
-<p>Pour le deuxième cas le plus fréquent avec la conjonction «&nbsp;et&nbsp;» qui coordonnent deux verbes&nbsp;:</p>
-
+<p>Pour le deuxième cas le plus fréquent avec la conjonction «&nbsp;et&nbsp;» qui coordonnent deux verbes&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Vraiment, j'y ai <span class="solution">passé</span> du bon temps et <span class="highlight">compte</span> y retourner !</span></li>
   </ul>
 </em>
+</p>
 
 <p>Pour le troisième cas, les conjonctions de coordination ne sont en revanche pas toujours nécessaires à la construction de coordonnés.</p>
-<p>Lorsque plusieurs coordinations se suivent la ponctuation leur est préférée, à noter que la tête est toujours le premier élément de l’énumération&nbsp;:</p>
+<p>Lorsque plusieurs coordinations se suivent la ponctuation leur est préférée, à noter que la tête est toujours le premier élément de l’énumération&nbsp;:
 <em>
   <ul>
     <li><span class="sentence">Une seconde d’<span class="solution">hésitation</span>, d’<span class="highlight">inattention</span> ou de retard</span></li>
   </ul>
 </em>
-
+</p>
 
 
