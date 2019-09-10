9797f7e6026345d0d1d5f300d1f2a4baa03f94c6
Karen Fort <kfort@schplaf.org>
2019-09-10 18:52:13 +0200

    modified help for ZLUD (conj_coord)

--- resources/views/help/fr/ud_cc.php
+++ resources/views/help/fr/ud_cc.php
@@ -1,30 +1,30 @@
-<p>La conjonction de coordination relie deux éléments de la phrase. Cette conjonction peut être simple (mais, ou, et, donc, or, ni, car) ou composée (parce que, à moins que, c'est-à-dire, etc.).</p>
+<p>La conjonction de coordination relie deux éléments de la phrase. Cette conjonction peut être simple (mais, ou, et, donc, or, ni, car) ou composée (parce que, à moins que, c'est-à-dire, etc.). Ici, il faut trouver l'élément coordonné à droite de la conjonction :</p>
 
 <em>
   <ul>
-    <li><span class="sentence">Les plats sont copieux <span class="solution">et</span> <span class="highlight">cuisinés</span> avec de bons produits.</span></li>
-    <li><span class="sentence"> Nous avons attendu sept <span class="solution">ou</span> <span class="highlight">huit</span> heures dans la gare.</span></li>
+    <li><span class="sentence">Les plats sont copieux <span class="highlight">et</span> <span class="solution">cuisinés</span> avec de bons produits.</span></li>
+    <li><span class="sentence"> Nous avons attendu sept <span class="highlight">ou</span> <span class="solution">huit</span> heures dans la gare.</span></li>
   </ul>
 </em>
 </p>
 <p>Attention, on peut aussi utiliser des conjonctions de coordination pour expliquer («&nbsp;c’est-à-dire&nbsp;»)&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="solution">c’est-à-dire</span> la <span class="highlight">cousine</span> de Zoé, est une astronaute</span>.</li>
+    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="highlight">c’est-à-dire</span> la <span class="solution">cousine</span> de Zoé, est une astronaute</span>.</li>
   </ul>
 </em>
 </p>
 <p>Pour nuancer («&nbsp;Mais&nbsp;»)&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence"><span class="solution">Mais</span> faut aussi être <span class="highlight">réaliste</span>.</span></li>
+    <li><span class="sentence"><span class="highlight">Mais</span> faut aussi être <span class="solution">réaliste</span>.</span></li>
   </ul>
 </em>
 </p>
 <p>Pour exclure une possibilité («&nbsp;Ni&nbsp;»)&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence"><span class="solution">Ni</span> <span class="highlight">toi</span> ni lui n’êtes arrivés au bout de la course.</span></li>
+    <li><span class="sentence"><span class="highlight">Ni</span> <span class="solution">toi</span> ni lui n’êtes arrivés au bout de la course.</span></li>
   </ul>
 </em>
 </p>
