d8aefe0e91251bf7364a4ad008049f8dd22d10b2
Karen Fort <kfort@schplaf.org>
2019-09-10 18:30:32 +0200

    modified help for ZLUD (cc)

--- resources/views/help/fr/ud_cc.php
+++ resources/views/help/fr/ud_cc.php
@@ -1,28 +1,27 @@
-<p>La conjonction de coordination relie deux éléments coordonnés, de même statut ou fonction, entre eux. Elle peut être simple (mais, ou, et, donc, or, ni, car) ou composée (parce que, à moins que, c'est-à-dire, etc.).</p>
+<p>La conjonction de coordination relie deux éléments de la phrase. Cette conjonction peut être simple (mais, ou, et, donc, or, ni, car) ou composée (parce que, à moins que, c'est-à-dire, etc.).</p>
 
-<p>En français, les conjonctions peuvent apparaître de la manière suivante par fréquence d’apparition&nbsp;: Un énoncé où l’on inclut plusieurs possibilités peut également être une relation avec une conjonction de coordination&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence">Vraiment très bon resto avec un accueil aussi chaleureux qu'enthousiaste... Les plats sont copieux <span class="solution">et</span> <span class="highlight">cuisinés</span> avec de bons produits.</span></li>
+    <li><span class="sentence">Les plats sont copieux <span class="solution">et</span> <span class="highlight">cuisinés</span> avec de bons produits.</span></li>
     <li><span class="sentence"> Nous avons attendu sept <span class="solution">ou</span> <span class="highlight">huit</span> heures dans la gare.</span></li>
   </ul>
 </em>
 </p>
-<p>Attention, certaines fois des relations peuvent exister avec des expressions du type «&nbsp;c’est-à-dire&nbsp;».
+<p>Attention, on peut aussi utiliser des conjonctions de coordination pour expliquer («&nbsp;c’est-à-dire&nbsp;»)&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="solution">c’est-à-dire</span> la <span class="highlight">cousine</span> de Zoé, est une astronaute</span></li>
+    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="solution">c’est-à-dire</span> la <span class="highlight">cousine</span> de Zoé, est une astronaute</span>.</li>
   </ul>
 </em>
 </p>
-<p>Dans un cas où l’on nuance comme celui-ci&nbsp;:
+<p>Pour nuancer («&nbsp;Mais&nbsp;»)&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class="solution">Mais</span> faut aussi être <span class="highlight">réaliste</span>.</span></li>
   </ul>
 </em>
 </p>
-<p>Dans un énoncé où l’on exclut une possibilité&nbsp;:
+<p>Pour exclure une possibilité («&nbsp;Ni&nbsp;»)&nbsp;:
 <em>
   <ul>
     <li><span class="sentence"><span class="solution">Ni</span> <span class="highlight">toi</span> ni lui n’êtes arrivés au bout de la course.</span></li>
