26bf4be9f0b0b693492977a8193eaab071ae8d26
Karen Fort <kfort@schplaf.org>
2019-09-10 18:21:01 +0200

    modified help for ZLUD (obj)

--- resources/views/help/fr/ud_obj.php
+++ resources/views/help/fr/ud_obj.php
@@ -1,11 +1,11 @@
-<p>Un objet direct est l’objet du verbe.
+<p>Un objet direct est l'objet du verbe : 
 <em>
   <ul>
     <li><span class="sentence">on <span class="highlight">voit</span> une jeune <span class="solution">fille</span> habillée en noir.</span></li>
   </ul>
 </em>
 </p>
-<p>Il peut également être un pronom réfléchi dans un verbe pronominal lorsque le pronom est un objet direct du verbe.
+<p>Il peut également être un pronom réfléchi dans un verbe pronominal lorsque le pronom est un objet direct du verbe :
 <em>
   <ul>
     <li><span class="sentence">il <span class="solution">se</span> <span class="highlight">lave</span>.</span></li>
@@ -13,21 +13,14 @@
 </em>
 </p>
 
-<p>Lorsque le pronom fait partie d‘une expression verbale fixe, il est également considéré en tant qu‘objet direct (<span class="sentence"><span class="solution">se</span> <span class="highlight">douter</span></span>, <span class="sentence"><span class="solution">se</span> <span class="solution">souvenir</span>, etc.).</span></p>
+<p>Lorsque le pronom fait partie d'une expression verbale fixe, il est également considéré en tant qu'objet direct (<span class="sentence"><span class="solution">se</span> <span class="highlight">douter</span>, <span class="solution">se</span> <span class="solution">souvenir</span>, etc.).</span></p>
 
-<p>Afin de pouvoir déterminer si un élément est un objet direct, on utilise un test dit d’interrogation, on pose une question de manière à retrouver l’élément qui est affecté par le verbe.
+<p>En cas de doute, on peut utiliser un test d’interrogation : on pose une question de manière à trouver ce qui est affecté par le verbe. Dans l'exemple ci-dessous, «&nbsp;qu'est-ce qui a été repris&nbsp;» ?
 <em>
   <ul>
     <li><span class="sentence">En 1872, quand les Georgiens ont <span class="highlight">repris</span> les <span class="solution">commandes</span> du gouvernement, Barnett (qui avait été réélu par ce point) a rapporté le sceau.</span></li>
   </ul>
 </em>
 </p>
-<p>La question que l’on peut poser est «&nbsp;qu’est-ce qui a été repris&nbsp;» ?
 
-<em>
-  <ul>
-<li><span class="sentence">L‘album de photos de police exposé au musée d‘Histoire politique de la Russie de Saint-Pétersbourg <span class="highlight">révèle</span> le <span class="solution">visage</span> de Raspoutine défoncé par des coups et son corps avec quatre points d‘impacts de balles qui ont traversé le cœur, le cou et le cerveau.</span></li>
-  </ul>
-</em>
-</p>
-<p>La question que l’on peut poser est «&nbsp;que révèle l’album de photos ?&nbsp;»</p>
+
