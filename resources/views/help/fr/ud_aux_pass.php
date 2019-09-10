89186965c10ff94336ef50816ee5f6d2bbbdc0c6
Karen Fort <kfort@schplaf.org>
2019-09-10 17:29:37 +0200

    modified help for ZLUD (aux_pass)

--- resources/views/help/fr/ud_aux_pass.php
+++ resources/views/help/fr/ud_aux_pass.php
@@ -1,24 +1,15 @@
-<p>Attention, ici il ne faut sélectionner que l'auxiliaire passif, si l'auxiliaire ne permet pas de rendre compte d'une phrase au passif, il faut mettre croix d'os</p>
-<p>Un auxiliaire passif n‘est pas le verbe principal d‘une proposition mais contient l‘information que le sujet subit l‘action exprimée par le verbe principal.</p>
-<p>Seul <em>être</em> peut être utilisé comme auxiliaire passif.
+<p>Seul <em>être</em> peut être utilisé comme auxiliaire passif :
 <em>
   <ul>
-    <li><span class="sentence">Kennedy <span class="solution">fut</span> <span class="highlight">assassiné</span></span></li>
+    <li><span class="sentence">Kennedy <span class="solution">fut</span> <span class="highlight">assassiné</span> (sous-entendu, "par quelqu'un").</span></li>
   </ul>
 </em>
 </p>
-<p>Dans un cas particulier on peut avoir deux auxiliaires rattachés au même mot&nbsp;:
+<p>Attention, dans certains cas, on peut avoir deux auxiliaires rattachés au même mot&nbsp;:
 <em>
   <ul>
-    <li><span class="sentence">Kennedy a <span class="solution">été</span> <span class="highlight">assassiné</span> (ici a est l‘auxiliaire, été est l‘auxiliaire passif et assassiné est le verbe principal)</span></li>
+    <li><span class="sentence">Kennedy a <span class="solution">été</span> <span class="highlight">assassiné</span>. (ici <em>a</em> est l‘auxiliaire, <em>été</em> est l‘auxiliaire passif et <em>assassiné</em> est le verbe principal)</span></li>
   </ul>
 </em>
 </p>
-<p>Dans tous les cas c’est le verbe être qui est l’auxiliaire passif.</p>
-<p>Il n’est possible d’avoir un auxiliaire passif qu’avec les verbes transitifs, ce sont des verbes qui admettent un ou plusieurs compléments d’objets.
-<em>
-  <ul>
-    <li><span class="sentence">Le détroit <span class="solution">est</span> <span class="highlight">traversé</span> par deux ponts routiers&nbsp;: le Carquinez Bridge, sur lequel passe l‘Interstate 80, et le Benicia-Martinez Bridge emprunté par l‘Interstate 680.</span></li>
-  </ul>
-</em>
-</p>
+
