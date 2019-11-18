d983784a4e6458610d8cd0f3273952ac5e3dd8d4
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2019-11-18 11:37:46 +0100

    review cc help file

--- resources/views/help/fr/ud_cc.php
+++ resources/views/help/fr/ud_cc.php
@@ -1,30 +1,15 @@
-<p>La conjonction de coordination relie deux éléments de la phrase. Cette conjonction peut être simple (mais, ou, et, donc, or, ni, car) ou composée (parce que, à moins que, c'est-à-dire, etc.). Ici, il faut trouver l'élément coordonné à droite de la conjonction :</p>
-
+<p>La conjonction de coordination relie deux éléments de la phrase. Cette conjonction peut être simple (<em>et</em>, <em>ou</em>, <em>mais</em>, <em>ni</em>…) ou composée (<em>parce que</em>, <em>à moins que</em>, <em>c'est-à-dire</em>…). Ici, il faut trouver l'élément coordonné à droite de la conjonction&nbsp;:</p>
 <em>
   <ul>
     <li><span class="sentence">Les plats sont copieux <span class="highlight">et</span> <span class="solution">cuisinés</span> avec de bons produits.</span></li>
-    <li><span class="sentence"> Nous avons attendu sept <span class="highlight">ou</span> <span class="solution">huit</span> heures dans la gare.</span></li>
+    <li><span class="sentence">Nous avons attendu sept <span class="highlight">ou</span> <span class="solution">huit</span> heures dans la gare.</span></li>
+    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="highlight">c’est-à-dire</span> la <span class="solution">cousine</span> de Zoé, est une astronaute.</span></li>
   </ul>
 </em>
-</p>
-<p>Attention, on peut aussi utiliser des conjonctions de coordination pour expliquer («&nbsp;c’est-à-dire&nbsp;»)&nbsp;:
+
+<p>La conjonction de coordination peut être en début de phrase&nbsp;:</p>
 <em>
   <ul>
-    <li><span class="sentence">La fille du cousin du père de Zoé, <span class="highlight">c’est-à-dire</span> la <span class="solution">cousine</span> de Zoé, est une astronaute</span>.</li>
+    <li><span class="highlight">Et</span> l'acte que j'accomplis ici n'est qu'un <span class="solution">moyen</span> révolutionnaire…</li>
   </ul>
 </em>
-</p>
-<p>Pour nuancer («&nbsp;Mais&nbsp;»)&nbsp;:
-<em>
-  <ul>
-    <li><span class="sentence"><span class="highlight">Mais</span> faut aussi être <span class="solution">réaliste</span>.</span></li>
-  </ul>
-</em>
-</p>
-<p>Pour exclure une possibilité («&nbsp;Ni&nbsp;»)&nbsp;:
-<em>
-  <ul>
-    <li><span class="sentence"><span class="highlight">Ni</span> <span class="solution">toi</span> ni lui n’êtes arrivés au bout de la course.</span></li>
-  </ul>
-</em>
-</p>
